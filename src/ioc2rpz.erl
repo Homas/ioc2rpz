eece21b09f7dd781528ad89bc6410ca1da25909e
Vadim Pavlov <13776103+Homas@users.noreply.github.com>
2019-07-05 18:26:51 -0700

    ioc2rpz is a place where threat intelligence meets DNS

--- src/ioc2rpz.erl
+++ src/ioc2rpz.erl
@@ -146,7 +146,7 @@ parse_dns_request(Socket, Data, Proto) when byte_size(Data) =< 12 ->
   ioc2rpz_fun:logMessageCEF(ioc2rpz_fun:msg_CEF(101),[ip_to_str(Proto#proto.rip),Proto#proto.rport,Proto#proto.proto]);
 
 parse_dns_request(Socket, Data, Proto) when Proto#proto.rport == 53; Proto#proto.rport == 853 ->
-%%% DDoS attemt
+%%% DDoS attempt
   ioc2rpz_fun:logMessageCEF(ioc2rpz_fun:msg_CEF(501),[ip_to_str(Proto#proto.rip),Proto#proto.rport,Proto#proto.proto]);
 
   
