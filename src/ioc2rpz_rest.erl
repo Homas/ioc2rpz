a73bf557a10e6d7bee59c14b567bcbe4cf5ae2a5
Vadim Pavlov <13776103+Homas@users.noreply.github.com>
2019-07-24 18:22:23 -0700

    ioc2rpz is a place where threat intelligence meets DNS

--- src/ioc2rpz_rest.erl
+++ src/ioc2rpz_rest.erl
@@ -1,4 +1,19 @@
+%Copyright 2017-2019 Vadim Pavlov ioc2rpz[at]gmail[.]com
+%
+%Licensed under the Apache License, Version 2.0 (the "License");
+%you may not use this file except in compliance with the License.
+%You may obtain a copy of the License at
+%
+%    http://www.apache.org/licenses/LICENSE-2.0
+%
+%Unless required by applicable law or agreed to in writing, software
+%distributed under the License is distributed on an "AS IS" BASIS,
+%WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%See the License for the specific language governing permissions and
+%limitations under the License.
+
 -module(ioc2rpz_rest).
+-include_lib("eunit/include/eunit.hrl").
 
 -include_lib("ioc2rpz.hrl").
 
@@ -263,3 +278,8 @@ ioc2jsonarr(Resp,[[IOC|_]|REST]) ->
 
 ioc2jsonarr(Resp,[]) ->
     Resp.
+
+
+%%%%
+%%%% EUnit tests
+%%%%
