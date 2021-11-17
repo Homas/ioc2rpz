f4df0318eb190255801e25945425a551f379e6b0
Vadim Pavlov <13776103+Homas@users.noreply.github.com>
2021-11-16 21:30:55 -0800

    bump version

--- include/ioc2rpz.hrl
+++ include/ioc2rpz.hrl
@@ -48,7 +48,7 @@
 %%%%%%
 %%%%%% Do not modify any settings below the line
 %%%%%%
--define(ioc2rpz_ver, "1.2.0.0-2021073101").
+-define(ioc2rpz_ver, "1.2.0.1-2021111601").
 
 -define(ZNameZip,16#c00c:16). %Zone name/original fqdn from a request is always at byte 10 in the response
 -define(ZNameZipN,16#c00c). % Offset in bytes - Zone name/original fqdn from a request is always at byte 10 in the response
